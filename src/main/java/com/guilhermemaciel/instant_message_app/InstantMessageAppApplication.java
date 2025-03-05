c6a4483b4b9c00a53e962f5a6944c72921b20bc5
Dyastrophism <guilhermesouzamaciel1@gmail.com>
2025-03-05 16:53:30 -0300

    enviroment preparation

---
+++ src/main/java/com/guilhermemaciel/instant_message_app/InstantMessageAppApplication.java
@@ -0,0 +1,13 @@
+package com.guilhermemaciel.instant_message_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstantMessageAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstantMessageAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/guilhermemaciel/instant_message_app/InstantMessageAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guilhermemaciel.instant_message_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstantMessageAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
