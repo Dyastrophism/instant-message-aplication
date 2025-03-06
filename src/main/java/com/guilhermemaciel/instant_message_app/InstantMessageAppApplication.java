807c453a23470d366003a461fdb8722dbe3cd91b
Dyastrophism <guilhermesouzamaciel1@gmail.com>
2025-03-06 10:10:10 -0300

    adding base entity

--- src/main/java/com/guilhermemaciel/instant_message_app/InstantMessageAppApplication.java
+++ src/main/java/com/guilhermemaciel/instant_message_app/InstantMessageAppApplication.java
@@ -2,8 +2,10 @@ package com.guilhermemaciel.instant_message_app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class InstantMessageAppApplication {
 
 	public static void main(String[] args) {
